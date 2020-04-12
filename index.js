4cd35949c8c3853c2575fe3872bf9e2847dbe423
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:50:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// Code your solution here
+let drivers = [{
+    "name": "tom"
+  },
+  {
+    "name": "bobby"
+  },
+  {
+    "name": "mike"
+  },
+  {
+    "name": "jeremy"
+  },
+  {
+    "name": "richard"
+  },
+]
+
 const findMatching = (drivers, string) => {
 let foundDrivers = drivers.filter(driver => {
   return driver.toLowerCase() === string.toLowerCase()
