52440aec0223cfd999489f91c4d0cdd4394cea9e
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Code your solution here
-const findMatching = (drivers) => {
-let string;
+const findMatching = (drivers, string) => {
+let foundDrivers = 
 drivers.filter(string => {
   return string === 'Bobby';
 })
+}
+
+const findMatching = (drivers, string) => {
+  let foundDrivers = drivers.filter(driver => {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+  return foundDrivers
 }
 
