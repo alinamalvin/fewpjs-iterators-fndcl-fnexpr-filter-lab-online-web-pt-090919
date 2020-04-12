29a55436612eed867b5693f2be61fd8dc44909a4
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const array = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-const string = 'Bobby';
-
 function findMatching(drivers, string){
   return drivers.filter(e => {
     return e.toLowerCase() === string.toLowerCase()
   })
 }
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(str => {
+    return str[0].toLowerCase() === string[0].toLowerCase()
+  })
+}
+
+function matchName(drivers, string){
+  return drivers.filter(str => {
+    return str.name.toLowerCase() === string.toLowerCase()
+  })
 }
 
 
