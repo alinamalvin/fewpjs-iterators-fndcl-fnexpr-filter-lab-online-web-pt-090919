97d63b5b6c4379da58dac2b237ab7c8511e68406
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:54:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const array = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
 const string = 'Bobby';
 
-function findMatching(array, driver) {
-    return array.filter( d => {
-        return d.toLowerCase() == driver.toLowerCase()
-    }) 
+function findMatching(drivers, string){
+  return drivers.filter(e => {
+    return e.toLowerCase() === string.toLowerCase()
+  })
+}
 }
 
 
